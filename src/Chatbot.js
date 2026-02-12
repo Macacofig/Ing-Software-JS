78e434c908cae33f4cca1be0e4d8d627201ce1ad
omkrfig <omkrfig@gmail.com>
2026-02-12 10:25:50 -0400

    feat: Saludo adaptativo segun la hora, muestra la hora y nombre del usuario

--- src/Chatbot.js
+++ src/Chatbot.js
@@ -2,4 +2,15 @@ function saludar(nombre) {
     return `Hola ${nombre}`;
 }
 
-export {saludar};
+function SaludohoraActual(nombre) {
+    const fecha = new Date();
+    const horaFormateada = fecha.toLocaleTimeString();
+    if (fecha.getHours() < 12) {
+        return `Buenos días ${nombre}, son las ${horaFormateada}`;
+    } else if (fecha.getHours() < 18) {
+        return `Buenas tardes ${nombre}, son las ${horaFormateada}`;
+    } else {
+        return `Buenas noches ${nombre}, son las ${horaFormateada}`;
+    }
+}
+export {saludar, SaludohoraActual};
--- src/presenter-chatbot.js
+++ src/presenter-chatbot.js
@@ -1,10 +1,16 @@
-import {saludar} from "./Chatbot.js";
+import {saludar, SaludohoraActual} from "./Chatbot.js";
 
 const nombre = document.getElementById("nombre");
 const form = document.getElementById("chat-form");
 const div = document.getElementById("chat-output");
 
+/*
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     div.innerHTML =  "<p>" + saludar(nombre.value) + "</p>";
+});*/
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    div.innerHTML =  "<p>" + SaludohoraActual(nombre.value) + "</p>";
 });
