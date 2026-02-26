e1d2e732b6d023391670759ee4871b23054af466
omkrfig <omkrfig@gmail.com>
2026-02-26 09:41:50 -0400

    feat: TennisGame Player 1 has 1 point

--- src/TenisGame.js
+++ src/TenisGame.js
@@ -9,6 +9,20 @@ class TenisGame {
     showScore() {
         return `${this.scorePlayer1}-${this.scorePlayer2}`;
     }
+
+    point(player) {
+        if (player === "Player 1") {
+            this.scorePlayer1 = this.nextScore(this.scorePlayer1);
+        } else if (player === "Player 2") {
+            this.scorePlayer2 = this.nextScore(this.scorePlayer2);
+        }
+    }
+    
+    nextScore(ActualScore) {
+        const scores = ["Love", "15"];
+        const currentIndex = scores.indexOf(ActualScore);
+        return scores[currentIndex + 1];
+    }
 }
 
 export {TenisGame};
--- src/TenisGame.test.js
+++ src/TenisGame.test.js
@@ -1,8 +1,13 @@
 import {TenisGame} from "./TenisGame.js";
 
 describe("Tennis Game", () => {
-  it("Deberia ser Love-Love al comentar el juego", () => {
+  it("Deberia ser Love-Love al comenzar el juego", () => {
     const marcador = new TenisGame();
     expect(marcador.showScore()).toEqual("Love-Love");
+  });
+  it("Deberia ser 15-Love al marcar un punto para el jugador 1", () => {
+    const marcador = new TenisGame();
+    marcador.point("Player 1");
+    expect(marcador.showScore()).toEqual("15-Love");
   });
 });
